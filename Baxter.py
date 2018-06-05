bfa9cab0c8d430e924fe306ead02c47a4d58cba1
Raghav Rao <raghav.rao32@gmail.com>
2018-06-05 16:29:22 -0400

    initial commit -- Working Baxter and ROS

---
+++ Baxter.py
@@ -0,0 +1,384 @@
+#!/usr/bin/python
+import sys
+import time 
+from PhidgetHelperFunctions import *
+from Phidget22.Devices.VoltageInput import *
+from Phidget22.PhidgetException import *
+from Phidget22.Phidget import *
+from Phidget22.Net import *
+import rospy
+from std_msgs.msg import String
+
+
+"""
+* Configures the device's DataInterval and ChangeTrigger.
+* Displays info about the attached phidget channel.
+* Fired when a Phidget channel with onAttachHandler registered attaches
+*
+* @param self The Phidget channel that fired the attach event
+"""
+def onAttachHandler(self):
+    
+    ph = self
+
+    """
+    * Set the DataInterval inside of the attach handler to initialize the device with this value.
+    * DataInterval defines the minimum time between VoltageChange events.
+    * DataInterval can be set to any value from MinDataInterval to MaxDataInterval.
+    """
+    print("\tSetting DataInterval to 1000ms")
+    try:
+        ph.setDataInterval(100)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting DataInterval: \n\t")
+        DisplayError(e)
+        return
+
+    """
+    * Set the VoltageChangeTrigger inside of the attach handler to initialize the device with this value.
+    * VoltageChangeTrigger will affect the frequency of VoltageChange events, by limiting them to only occur when
+    * the voltage changes by at least the value set.
+    """
+    print("\tSetting Voltage ChangeTrigger to 0.0")
+    try:
+        ph.setVoltageChangeTrigger(0.0)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Set VoltageChangeTrigger: \n\t")
+        DisplayError(e)
+        return
+
+    """
+    * Set the SensorType inside of the attach handler to initialize the device with this value.
+    * SensorType will apply the appropriate calculations to the voltage reported by the device
+    * to convert it to the sensor's units.
+    * SensorType can only be set for Sensor Port voltage inputs (VINT Ports and Analog Input Ports)
+    """
+    if(ph.getChannelSubclass() == ChannelSubclass.PHIDCHSUBCLASS_VOLTAGEINPUT_SENSOR_PORT):
+        print("\tSetting Voltage SensorType")
+        try:
+            ph.setSensorType(VoltageSensorType.SENSOR_TYPE_VOLTAGE)
+        except PhidgetException as e:
+            sys.stderr.write("Runtime Error -> Set SensorType: \n\t")
+            DisplayError(e)
+            return
+        
+    try:
+        serialNumber = ph.getDeviceSerialNumber()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get DeviceSerialNumber: \n\t")
+        DisplayError(e)
+        return
+
+    try:
+        channel = ph.getChannel()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get Channel: \n\t")
+        DisplayError(e)
+        return
+
+    # Check if this is a VINT device
+    try:
+        hub = ph.getHub()
+    except PhidgetException as e:
+        if(e.code == ErrorCode.EPHIDGET_WRONGDEVICE):
+            print("\nAttach Event:\n\t-> Serial Number: " + str(serialNumber) +
+                    "\n\t-> Channel " + str(channel) + "\n")
+        return
+    try:
+        hubPort = ph.getHubPort()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get HubPort: \n\t")
+        DisplayError(e)
+        return
+
+    print("\nAttach Event:\n\t-> Serial Number: " + str(serialNumber) +
+          "\n\t-> Hub Port: " + str(hubPort) + "\n\t-> Channel " + str(channel) + "\n")
+    return
+
+"""
+* Displays info about the detached phidget channel.
+* Fired when a Phidget channel with onDetachHandler registered detaches
+*
+* @param self The Phidget channel that fired the attach event
+"""
+def onDetachHandler(self):
+
+    ph = self
+
+    try:
+        serialNumber = ph.getDeviceSerialNumber()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get DeviceSerialNumber: \n\t")
+        DisplayError(e)
+        return
+
+    try:
+        channel = ph.getChannel()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get Channel: \n\t")
+        DisplayError(e)
+        return
+
+     # Check if this is a VINT device
+    try:
+        hub = ph.getHub()
+    except PhidgetException as e:
+        if(e.code == ErrorCode.EPHIDGET_WRONGDEVICE):
+            print("\nDetach Event:\n\t-> Serial Number: " + str(serialNumber) +
+                    "\n\t-> Channel " + str(channel) + "\n")
+        return
+
+    try:
+        hubPort = ph.getHubPort()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Get HubPort: \n\t")
+        DisplayError(e)
+        return
+
+    print("\nDetach Event:\n\t-> Serial Number: " + str(serialNumber) +
+          "\n\t-> Hub Port: " + str(hubPort) + "\n\t-> Channel " + str(channel) + "\n")
+    return
+
+"""
+* Writes phidget error info to stderr.
+* Fired when a Phidget channel with onErrorHandler registered encounters an error in the library
+*
+* @param self The Phidget channel that fired the attach event
+* @param errorCode the code associated with the error of enum type ph.ErrorEventCode
+* @param errorString string containing the description of the error fired
+"""
+def onErrorHandler(self, errorCode, errorString):
+
+    sys.stderr.write("[Phidget Error Event] -> " + errorString + " (" + str(errorCode) + ")\n")
+
+"""
+* Sets the event handlers for Phidget Attach, Phidget Detach, Phidget Error events
+*
+* @param ph The Phidget channel to add event handlers to
+* @return if the operation succeeds
+* @raise PhidgetException if it fails
+"""
+def SetAttachDetachError_Handlers(ph):
+    print("\n--------------------------------------")
+    print("\nSetting OnAttachHandler...")
+    try:
+        ph.setOnAttachHandler(onAttachHandler)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Set Attach Handler: \n\t")
+        DisplayError(e)
+        raise
+
+    print("Setting OnDetachHandler...")
+    try:
+        ph.setOnDetachHandler(onDetachHandler)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Set Detach Handler: \n\t")
+        DisplayError(e)
+        raise
+
+    print("Setting OnErrorHandler...")
+    try:
+        ph.setOnErrorHandler(onErrorHandler)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Set Error Handler: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+
+def talker(voltage, publisher):
+    global volts
+    volts =  str(voltage)
+    hello_str = volts
+#    rospy.loginfo(hello_str)
+    publisher.publish(hello_str)
+
+"""
+* Outputs the VoltageInput's most recently reported voltage.
+* Fired when a VoltageInput channel with onVoltageChangeHandler registered meets DataInterval and ChangeTrigger criteria
+*
+* @param self The VoltageInput channel that fired the VoltageChange event
+* @param voltage The reported voltage from the VoltageInput channel
+"""
+def onVoltageChangeHandler(self, voltage):
+
+    global volts
+    volts = str(voltage)
+    print("[Voltage Event] -> Voltage: " + volts)
+    
+"""
+* Outputs the VoltageInput's most recently reported sensor value.
+* Fired when a VoltageInput channel with onSensorChangeHandler registered meets DataInterval and ChangeTrigger criteria
+*
+* @param self The VoltageInput channel that fired the SensorChange event
+* @param sensorValue The reported sensor value from the VoltageInput channel
+"""
+def onSensorChangeHandler(self, sensorValue, sensorUnit):
+
+    print("[Sensor Event] -> Sensor Value: " + str(sensorValue) + sensorUnit.symbol)
+    
+"""
+* Creates a new instance of a VoltageInput channel.
+*
+* @param pvih Pointer to the PhidgetVoltageInputHandle channel to create
+* @return if the operation succeeds
+* @raise PhidgetException if it fails
+"""
+def CreateVoltageInput(pvih):
+
+    print("Creating VoltageInput Channel...")
+    try:
+        pvih.create()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Creating VoltageInput: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+"""
+* Sets the event handler for VoltageInput's VoltageChange event
+*
+* @param pvih The PhidgetVoltageInputHandle channel to add the event to
+* @param fptr The callback function to be called when a VoltageChange event is fired
+* @return if the operation succeeds
+* @raise PhidgetException if it fails
+"""
+def SetVoltageHandler(pvih, fptr):
+
+    if (not (fptr is None)):
+        print("\n--------------------\n"
+            "\n  | Voltage change events contain the most recent voltage received from the device.\n"
+            "  | The linked VoltageChange function will run as an event at every DataInterval.\n"
+            "  | These events will not occur until a change in voltage >= to the set ChangeTrigger has occurred.\n"
+            "  | DataInterval and ChangeTrigger should initially be set in the device AttachHandler function.")
+    
+    print("")
+    if (fptr is None):
+        print("Clearing OnVoltageChangeHandler...")
+    else:
+        print("Setting OnVoltageChangeHandler...")
+
+    print("\n--------------------")
+    try:
+        pvih.setOnVoltageChangeHandler(fptr)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting VoltageChangeHandler: \n\t")
+        DisplayError(e)
+        raise
+    
+    return
+    
+"""
+* Sets the event handler for VoltageInput's SensorChange event
+*
+* @param pvih The PhidgetVoltageInputHandle channel to add the event to
+* @param fptr The callback function to be called when a SensorChange event is fired
+* @return if the operation succeeds
+* @raise PhidgetException if it fails
+"""
+def SetSensorHandler(pvih, fptr):
+
+    if (not (fptr is None)):
+        print("\n--------------------\n"
+            "\n  | Sensor change events contain the most recent sensor value received from the device.\n"
+            "  | Sensor change events will occur instead of Voltage change events if the SensorType is changed from the default."
+            "  | The linked SensorChange function will run as an event at every DataInterval.\n"
+            "  | These events will not occur until a change in sensor value >= to the set ChangeTrigger has occurred.\n"
+            "  | DataInterval, ChangeTrigger and SensorType should initially be set in the device AttachHandler function.")
+    
+    print("")
+    if (fptr is None):
+        print("Clearing OnSensorChangeHandler...")
+    else:
+        print("Setting OnSensorChangeHandler...")
+
+    print("\n--------------------")
+    try:
+        pvih.setOnSensorChangeHandler(fptr)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting SensorChangeHandler: \n\t")
+        DisplayError(e)
+        raise
+    return
+
+"""
+* Creates, configures, and opens a VoltageInput channel.
+* Displays Voltage events for 10 seconds
+* Closes out VoltageInput channel
+*
+* @return 0 if the program exits successfully, 1 if it exits with errors.
+"""
+def main():
+    try:
+        """
+        * Allocate a new Phidget Channel object
+        """
+        try:
+            ch = VoltageInput()
+        except PhidgetException as e:
+            sys.stderr.write("Runtime Error -> Creating VoltageInput: \n\t")
+            DisplayError(e)
+            raise
+        except RuntimeError as e:
+            sys.stderr.write("Runtime Error -> Creating VoltageInput: \n\t" + e)
+            raise
+
+        """
+        * Set matching parameters to specify which channel to open
+        """
+        SetSerialNumber(ch)
+        
+        SetVINTProperties(ch)
+        
+        SetChannel(ch)
+        
+        SetupNetwork(ch)
+        
+        """
+        * Add event handlers before calling open so that no events are missed.
+        """
+        SetAttachDetachError_Handlers(ch)
+        
+        SetVoltageHandler(ch, onVoltageChangeHandler)
+        
+        SetSensorHandler(ch, onSensorChangeHandler)
+        
+        """
+        * Open the channel with a timeout
+        """
+        OpenPhidgetChannel_waitForAttach(ch, 5000)
+        
+        print("Sampling data for 10 seconds...")
+        pub = rospy.Publisher('/baxter/phidget/voltage', String, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            talker(volts, publisher=pub)
+            rate.sleep()
+
+        """
+         * Perform clean up and exit
+         """
+
+        SetVoltageHandler(ch, None)
+        
+        print("\nDone Sampling...")
+
+        print("Cleaning up...")
+        ClosePhidgetChannel(ch)
+        print("\nExiting...")
+        print("Press ENTER to end program.")
+        readin = sys.stdin.readline(1)
+        return 0
+
+    except PhidgetException as e:
+        sys.stderr.write("\nExiting with error(s)...")
+        DisplayError(e)
+        print("Press ENTER to end program.")
+        readin = sys.stdin.readline(1)
+        return 1
+
+main()
+
---
+++ PhidgetHelperFunctions.py
@@ -0,0 +1,387 @@
+import sys
+from Phidget22.PhidgetException import *
+from Phidget22.ErrorCode import *
+from Phidget22.Phidget import *
+from Phidget22.Net import *
+
+class InputError(Exception):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+# Returns None if an error occurred, True for 'Y' and False for 'N'  
+def ProcessYesNo_Input(default):
+
+    strvar = sys.stdin.readline(100)
+    if not strvar:
+        raise InputError("Empty Input String")
+
+    strvar = strvar.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+    if (strvar[0] == '\n'):
+        if (default == -1):
+            raise InputError("Empty Input String")
+        return default
+
+    if (strvar[0] == 'N' or strvar[0] == 'n'):
+        return False
+
+    if (strvar[0] == 'Y' or strvar[0] == 'y'):
+        return True
+
+    raise InputError("Invalid Input")
+
+def DisplayError(e):
+    sys.stderr.write("Desc: " + e.details + "\n")
+
+def SetSerialNumber(ph):
+    print("\nFor all questions, enter the value, or press ENTER to select the [Default]")
+
+    print("\n--------------------------------------")
+    print("\n  | Some Phidgets have a unique serial number, printed on a white label on the device.\n"
+      "  | For Phidgets and other devices plugged into a VINT Port, use the serial number of the VINT Hub.\n"
+      "  | Specify the serial number to ensure you are only opening channels from that specific device.\n"
+      "  | Otherwise, use -1 to open a channel on any device.")
+    while (True):
+        print("\nWhat is the Serial Number? [-1] ")
+        strvar = sys.stdin.readline(100)
+        if not strvar:
+            continue
+
+        strvar = strvar.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+        if (strvar[0] == '\n'):
+            deviceSerialNumber = -1
+            break
+
+        try:
+            deviceSerialNumber = int(strvar)
+        except ValueError as e:
+            continue
+
+        if (deviceSerialNumber >= -1 and deviceSerialNumber != 0):
+            break
+
+    try:
+        ph.setDeviceSerialNumber(deviceSerialNumber)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting DeviceSerialNumber: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+
+def SetIsHubPortDevice(ph):
+    isHubPortDevice = -1
+
+    while (True):
+        print("\nIs this a \"HubPortDevice\"? [y/n] ")
+        try:
+            isHubPortDevice = ProcessYesNo_Input(-1)
+            break
+        except InputError as e:
+            pass
+
+    try:
+        ph.setIsHubPortDevice(isHubPortDevice)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting isHubPortDevice: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+def SetVINTProperties(ph):
+    canBeHubPortDevice = 0
+    pcc = -1
+    hubPort = -1
+    isVINT = 0
+
+    print("\n--------------------------------------")
+
+    while (True):
+        print("\nDo you want to specify the hub port that your device is plugged into?\n"
+            "Choose No if your device is not plugged into a VINT Hub. (y/n) ")
+        try:
+            isVINT = ProcessYesNo_Input(-1)
+            break
+        except InputError as e:
+            pass
+
+    # Don't ask about the HubPort and the HubPortDevice if it's not a VINT device
+    if (not isVINT):
+        return
+
+    print("\n--------------------------------------")
+    print("\n  | VINT Hubs have numbered ports that can be uniquely addressed.\n"
+        "  | The HubPort# is identified by the number above the port it is plugged into.\n"
+        "  | Specify the hub port to ensure you are only opening channels from that specific port.\n"
+        "  | Otherwise, use -1 to open a channel on any port.")
+    while (True):
+        print("\nWhat HubPort is the device plugged into? [-1] ")
+        strvar = sys.stdin.readline(100)
+        if not strvar:
+            continue
+
+        strvar = strvar.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+        if (strvar[0] == '\n'):
+            hubPort = -1
+            break
+
+        try:
+            hubPort = int(strvar)
+        except ValueError as e:
+            continue
+
+        if (hubPort >= -1 and hubPort <= 5):
+            break
+
+    try:
+        ph.setHubPort(hubPort)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting HubPort: \n\t")
+        DisplayError(e)
+        raise
+
+    try:
+        pcc = ph.getChannelClass()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Getting ChannelClass: \n\t")
+        DisplayError(e)
+        raise
+
+    if (pcc == ChannelClass.PHIDCHCLASS_VOLTAGEINPUT):
+        print("\n--------------------------------------")
+        print("\n  | A VoltageInput HubPortDevice uses the VINT Hub's internal channel to measure the voltage on the white wire.\n"
+          "  | If the device you are trying to interface returns an analog voltage between 0V-5V, open it as a HubPortDevice.")
+        canBeHubPortDevice = 1
+    elif (pcc == ChannelClass.PHIDCHCLASS_VOLTAGERATIOINPUT):
+        print("\n--------------------------------------")
+        print("\n  | A VoltageRatioInput HubPortDevice uses the VINT Hub's internal channel to measure the voltage ratio on the white wire.\n"
+          "  | If the device you are trying to interface returns an ratiometric voltage between 0V-5V, open it as a HubPortDevice.")
+        canBeHubPortDevice = 1
+    elif (pcc == ChannelClass.PHIDCHCLASS_DIGITALINPUT):
+        print("\n--------------------------------------")
+        print("\n  | A DigitalInput HubPortDevice uses the VINT Hub's internal channel to detect digital changes on the white wire.\n"
+          "  | If the device you are trying to interface outputs a 5V digital signal, open it as a HubPortDevice.")
+        canBeHubPortDevice = 1
+    elif (pcc == ChannelClass.PHIDCHCLASS_DIGITALOUTPUT):
+        print("\n--------------------------------------")
+        print("\n  | A DigitalOutput HubPortDevice uses the VINT Hub's internal channel to output a 3.3V digital signal on the white wire.\n"
+          "  | If the device you are trying to interface accepts a 3.3V digital signal, open it as a HubPortDevice.")
+        canBeHubPortDevice = 1
+
+    if (canBeHubPortDevice):
+        SetIsHubPortDevice(ph)
+
+    return
+
+def SetChannel(ph):
+    isHubPortDevice = 0
+    Channel = 0
+
+    try:
+        isHubPortDevice = ph.getIsHubPortDevice()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Getting isHubPortDevice: \n\t")
+        DisplayError(e)
+        raise
+
+    # Hub port devices only have a single channel, so don't ask for the channel
+    if (isHubPortDevice):
+        return
+
+    print("\n--------------------------------------")
+    print("\n  | Devices with multiple inputs or outputs of the same type will map them to channels.\n"
+      "  | The API tab for the device on www.phidgets.com shows the channel breakdown.\n"
+      "  | For example, a device with 4 DigitalInputs would use channels [0 - 3]\n"
+      "  | A device with 1 VoltageInput would use channel 0")
+    while (True):
+        print("\nWhat channel# of the device do you want to open? [0] ")
+        strvar = sys.stdin.readline(100)
+        if not strvar:
+            continue
+
+        strvar = strvar.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+        if (strvar[0] == '\n'):
+            Channel = 0
+            break
+
+        try:
+            Channel = int(strvar)
+        except ValueError as e:
+            continue
+
+        if (Channel >= 0):
+            break
+
+    try:
+        ph.setChannel(Channel)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Setting Channel: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+def EnableServerDiscovery():
+    print("Enabling Server Discovery...")
+    try:
+        Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICEREMOTE)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Enable Server Discovery: \n\t")
+        DisplayError(e)
+        raise
+    return
+
+def SetupNetwork(ph):
+    hostname = ""
+    password = ""
+    discovery = 0
+    isRemote = 0
+    port = 0
+
+    print("\n--------------------------------------")
+    print("\n  | Devices can either be opened directly, or over the network.\n"
+      "  | In order to open over the network, the target system must be running a Phidget Server.")
+    while (True):
+        print("\nIs this device being opened over the network? [y/N] ")
+        try:
+            isRemote = ProcessYesNo_Input(0)
+            break
+        except InputError as e:
+            pass
+
+    try:
+        ph.setIsRemote(isRemote)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Set IsRemote: \n\t")
+        DisplayError(e)
+        raise
+
+    # if it's not remote, don't need to ask about the network
+    if (not isRemote):
+        return
+
+    print("\n--------------------------------------")
+    print("\n  | Server discovery enables the dynamic discovery of phidget servers that publish their identity to the network.\n"
+          "  | This allows you to open devices over the network without specifying the hostname and port of the server.")
+    while (True):
+        print("\nDo you want to enable server discovery? [Y/n] ");
+        try:
+            discovery = ProcessYesNo_Input(1)
+            break
+        except InputError as e:
+            pass
+
+    if (discovery):
+        return EnableServerDiscovery()
+
+    print("\n--------------------------------------")
+    print("\nPlease provide the following information in order to open the device")
+
+    while (True):
+        print("\nWhat is the Hostname (or IP Address) of the server? [localhost] ")
+        hostname = sys.stdin.readline(100)
+        if not hostname:
+            continue
+
+        hostname = hostname.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+        if (hostname[0] == '\n'):
+            hostname = "localhost"
+            break
+
+        # Remove trailing newline
+        hostname = hostname.split('\n')[0]
+        break
+
+    print("\n--------------------------------------")
+    while (True):
+        print("\nWhat port is the server on? [5661] ")
+        strvar = sys.stdin.readline(100)
+        if not strvar:
+            continue
+
+        strvar = strvar.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+        if (strvar[0] == '\n'):
+            port = 5661
+            break
+
+        try:
+            port = int(strvar)
+        except ValueError as e:
+            continue
+
+        if (port <= 65535 and port > 0):
+            break
+    
+    print("\n--------------------------------------")
+    while (True):
+        print("\nWhat is the password of the server? [] ")
+        password = sys.stdin.readline(100)
+        if not password:
+            continue
+        # Remove trailing newline
+        password = password.replace('\r\n', '\n') #sanitize newlines for Python 3.2 and older
+
+        password = password.split('\n')[0]
+        break
+    
+    print("\n--------------------------------------")
+
+    print("Adding Server...")
+    try:
+        Net.addServer("Server", hostname, port, password, 0)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> AddServer: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+def OpenPhidgetChannel(ph):
+    print("Opening Channel...")
+    try:
+        ph.open()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Opening Phidget Channel: \n\t")
+        DisplayError(e)
+        raise
+
+    return
+
+def OpenPhidgetChannel_waitForAttach(ph, timeout_in_ms):
+    print("Opening Channel...")
+    try:
+        ph.openWaitForAttachment(timeout_in_ms)
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Opening Phidget Channel: \n\t")
+        DisplayError(e)
+        if(e.code == ErrorCode.EPHIDGET_TIMEOUT):
+            sys.stderr.write("\nThis error commonly occurs if your device is not connected as specified, "
+                             "or if another program is using the device, such as the Phidget Control Panel.\n")
+            if(     ph.getChannelClass() != ChannelClass.PHIDCHCLASS_VOLTAGEINPUT
+                and ph.getChannelClass() != ChannelClass.PHIDCHCLASS_VOLTAGERATIOINPUT
+                and ph.getChannelClass() != ChannelClass.PHIDCHCLASS_DIGITALINPUT
+                and ph.getChannelClass() != ChannelClass.PHIDCHCLASS_DIGITALOUTPUT
+            ):
+                sys.stderr.write("\nIf you are trying to connect to an analog sensor, you will need to use the "
+                                 "corresponding VoltageInput or VoltageRatioInput API with the appropriate SensorType.\n")
+        raise
+
+    return
+
+def ClosePhidgetChannel(ph):
+    print("Closing Channel...")
+    try:
+        ph.close()
+    except PhidgetException as e:
+        sys.stderr.write("Runtime Error -> Closing Phidget Channel: \n\t")
+        DisplayError(e)
+        raise
+
+    return
